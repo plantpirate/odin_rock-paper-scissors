5bed2142404d20d283a81c89aa5d9a8177e0127e
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-07-17 12:05:47 -0500

    Add comments declaring what function does

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ console.log("hello jobin");
 // Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1. Think about how you can use this to conditionally return one of the multiple choices.
 
 function getComputerChoice() {
+  // create array with string options
   const choicesArr = ["rock", "paper", "scissors"];
+  // generate a random number up to length of const array
   const computerChoice = Math.floor(Math.random() * 3);
+  // return element from array using position from random number generated
   return choicesArr[computerChoice];
 }
 
