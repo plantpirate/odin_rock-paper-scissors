863908dd661f0ada61141a27728c59296a490c13
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-07-17 12:33:32 -0500

    Create playRound function

--- script.js
+++ script.js
@@ -30,5 +30,26 @@ function getHumanChoice() {
 // Create two new variables named humanScore and computerScore in the global scope.
 // Initialize those variables with the value of 0.
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+
+// Create a new function named playRound.
+// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
+// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
+// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+// Increment the humanScore or computerScore variable based on the round winner.
+
+function playRound(humanChoice, computerChoice) {
+  if (computerChoice === "rock" && humanChoice === "rock") {
+    console.log("Draw!");
+  } else if (computerChoice === "rock" && humanChoice === "paper") {
+    console.log("Paper beats rock!");
+  } else {
+    console.log("Rock beats scissors!");
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
