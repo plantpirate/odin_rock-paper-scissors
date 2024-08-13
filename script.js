c2ff2531d01ca42e9e80e96e5308a5a16e102022
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-12 21:35:18 -0500

    Remove pasted instruction from TOP

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 "use strict";
 console.log("hello jobin");
 
-// Create two new variables named humanScore and computerScore in the global scope.
-// Initialize those variables with the value of 0.
-
-// Create a new function named getComputerChoice.
-// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-
-// Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1. Think about how you can use this to conditionally return one of the multiple choices.
-
 function getComputerChoice() {
   // create array with string options
   const choicesArr = ["rock", "paper", "scissors"];
@@ -22,19 +14,6 @@ function getComputerChoice() {
   getComputerChoice.previousChoice = computerChoice;
   return computerChoice;
 }
-
-// Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.
-
-// Create a new function named getHumanChoice.
-// Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
-// Hint: Use the prompt method to get the user’s input.
-// Test what your function returns by using console.log.
-
-// Create a new function named playRound.
-// Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
-// Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-// Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-// Increment the humanScore or computerScore variable based on the round winner.
 
 function playGame() {
   const results = document.querySelector(".results__container span");
